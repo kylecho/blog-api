fdd181721ddad0200fd5cc00b856a2458b15c288
Kyle Cho <kylecho.work@gmail.com>
2016-06-23 20:44:52 -0700

    add post endpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624032146) do
+ActiveRecord::Schema.define(version: 20160624033620) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.string   "category"
+    t.integer  "rating"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",      null: false
